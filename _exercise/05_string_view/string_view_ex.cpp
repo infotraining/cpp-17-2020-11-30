b3eb71c6b53d95d41a0af9fddb3610fd6aa22c09
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-12-02 12:09:33 +0100

    string_view - update

--- _exercise/05_string_view/string_view_ex.cpp
+++ _exercise/05_string_view/string_view_ex.cpp
@@ -32,28 +32,7 @@ std::vector<std::string_view> split_text(string_view text, string_view separator
 
     return tokens;
 }
-{
-    std::vector<std::string_view> tokens;   
 
-    auto pos1 = cbegin(text);
-
-    while(pos1 != cend(text))
-    {
-        auto pos2 = std::find_first_of(pos1, cend(text), cbegin(separators), cend(separators));
-
-        auto token_starts = &(*pos1);
-        auto token_length = pos2 - pos1;
-
-        tokens.emplace_back(token_starts, token_length);
-
-        if (pos2 == cend(text))
-            break;
-        
-        pos1 = std::next(pos2);
-    }
-
-    return tokens;
-}
 
 TEST_CASE("split with spaces")
 {
